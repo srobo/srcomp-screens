4b0c53f65b8c391cb2befb06637435432178a7e4
Tom Leese <tom@tomleese.me.uk>
2014-04-22 14:48:59 +0100

    Start work on sr JavaScript library

---
+++ scripts/sr.js
@@ -0,0 +1,69 @@
+var sr = (function(sr) {
+    "use strict";
+
+    sr.comp = (function() {
+        var apiRoot = "";
+        var detectCompApiRoot = function(callback) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "/comp-api/state");
+
+            xhr.addEventListener("load", function(e) {
+                if (e.target.status === 200) {
+                    callback("/comp-api");
+                } else {
+                    callback("");
+                }
+            });
+
+            xhr.addEventListener("error", function(e) {
+                callback("");
+            });
+
+            xhr.send();
+        };
+
+        var get = function(url, callback) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", apiRoot + url);
+            xhr.responseType = "json";
+
+            xhr.addEventListener("load", function(e) {
+                callback(e.target.response);
+            });
+        };
+
+        return {
+            "init": function(callback) {
+                detectCompApiRoot(function(root) {
+                    apiRoot = root;
+                    console.log("Detected Competition API root as:", apiRoot);
+                    callback();
+                });
+            },
+            "arenas": get.bind(undefined, "/arenas"),
+            "corner": function(index, callback) {
+                get("/corner/" + index, callback);
+            },
+            "matches": function(arena, numbers, callback) {
+                if (callback === undefined) {
+                    callback = numbers;
+                    numbers = undefined;
+                }
+
+                var url = "/matches/" + arena;
+                if (numbers) {
+                    url += "?numbers=" + numbers;
+                }
+
+                get(url, callback);
+            }
+        };
+    }());
+
+    sr.init = function(callback) {
+        console.log("Initialising SR…");
+        sr.comp.init(callback);
+    };
+
+    return sr;
+}(sr || {}));
